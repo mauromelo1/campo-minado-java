058ffd444238899ec2d61127bd4dd0b5cd59718c
Mauro Melo <mauromelo1@gmail.com>
2020-10-05 17:44:03 -0300

    Classe Tabuleiro, atributos e metodos

--- src/com/meujogo/cm/modelo/Campo.java
+++ src/com/meujogo/cm/modelo/Campo.java
@@ -125,5 +125,4 @@ public class Campo {
 			return "?";
 		}
 	} // fim toString
-
 }
--- src/com/meujogo/cm/modelo/Tabuleiro.java
+++ src/com/meujogo/cm/modelo/Tabuleiro.java
@@ -1,5 +1,46 @@
 package com.meujogo.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+
+	private int linhas;
+	private int colunas;
+	private int minas;
+
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+
+	} // fim construtor
+
+	private void gerarCampos() {
+
+		for (int l = 0; l < linhas; l++) {
+			for (int c = 0; c < colunas; c++) {
+				campos.add(new Campo(l, c));
+			} // for c
+		} // for l
+	} // fim gerarCampos
+
+	private void associarVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinho(c2);
+			} // for c2
+		} // for c1
+	} // fim associarOsVizinhos
+
+	private void sortearMinas() {
+
+	} // sortearMinas
 
 }
